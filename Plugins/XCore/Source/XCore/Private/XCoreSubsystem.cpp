48d574d1e338e8906f2075218391c3cadc7019ff
ANEWGALAXI <anewgalaxy@hotmail.com>
2024-11-14 19:39:05 -0500

    Setup the session interface delegates and handles

--- Plugins/XCore/Source/XCore/Private/XCoreSubsystem.cpp
+++ Plugins/XCore/Source/XCore/Private/XCoreSubsystem.cpp
@@ -6,12 +6,93 @@
 #include "OnlineSubsystem.h"
 #include "OnlineSubsystemUtils.h"
 
-UXCoreSubsystem::UXCoreSubsystem()
+UXCoreSubsystem::UXCoreSubsystem():
+	OnCreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(
+		this, &ThisClass::OnCreateSessionCompleted)),
+	OnFindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(
+		this, &ThisClass::OnFindSessionCompleted)),
+	OnJoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(
+		this, &ThisClass::OnJoinSessionCompleted)),
+	OnDestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(
+		this, &ThisClass::OnDestroySessionCompleted)),
+	OnStartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(
+		this, &ThisClass::OnStartSessionCompleted))
 {
+	
 	// Get and validate the online subsystem.
 	if (IOnlineSubsystem* Subsystem = Online::GetSubsystem(UObject::GetWorld()); Subsystem)
 
 		// Store a reference to the session interface.
 		SessionInterface = Subsystem->GetSessionInterface();
+	
+}
+
+void UXCoreSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::OnCreateSessionCompleted(FName SessionName, bool bWasSuccessful)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::FindSessions(int32 MaxSearchResults)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::OnFindSessionCompleted(bool bWasSuccessful)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::JoinSession(const FOnlineSessionSearchResult& SearchResult)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::OnJoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::DestroySession()
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::OnDestroySessionCompleted(FName SessionName, bool bWasSuccessful)
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::StartSession()
+{
+
+	
+	
+}
+
+void UXCoreSubsystem::OnStartSessionCompleted(FName SessionName, bool bWasSuccessful)
+{
+
+	
 	
 }
--- Plugins/XCore/Source/XCore/Public/XCoreSubsystem.h
+++ Plugins/XCore/Source/XCore/Public/XCoreSubsystem.h
@@ -17,13 +17,47 @@ class XCORE_API UXCoreSubsystem : public UGameInstanceSubsystem
 	GENERATED_BODY()
 
 public:
-
+	
 	UXCoreSubsystem();
+
+	//
+	// To handle session functionality.
+	//
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSessions(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SearchResult);
+	void DestroySession();
+	void StartSession();
 
 protected:
 
-private:
+	//
+	//	Internal callbacks for the delegates that we'll add to the Online Session Interface delegate list.
+	//	These don't need to be called outside this class.
+	//
+	void OnCreateSessionCompleted(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionCompleted(bool bWasSuccessful);
+	void OnJoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionCompleted(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionCompleted(FName SessionName, bool bWasSuccessful);
 
+private:
+	
 	IOnlineSessionPtr SessionInterface;
+
+	//
+	//	To add to the Online Session Interface delegate list.
+	//	We'll bind our XCoreSession internal callbacks to these.
+	//
+	FOnCreateSessionCompleteDelegate	OnCreateSessionCompleteDelegate;
+	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+	FOnFindSessionsCompleteDelegate		OnFindSessionsCompleteDelegate;
+	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+	FOnJoinSessionCompleteDelegate		OnJoinSessionCompleteDelegate;
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+	FOnDestroySessionCompleteDelegate	OnDestroySessionCompleteDelegate;
+	FDelegateHandle OnDestroySessionCompleteDelegateHandle;
+	FOnStartSessionCompleteDelegate		OnStartSessionCompleteDelegate;
+	FDelegateHandle OnStartSessionCompleteDelegateHandle;
 	
 };
